0a4005b1e73f0810543b5f287f37de64f1f3984b
SebastianCarai <sebastiancarai7@gmail.com>
2022-01-24 15:08:09 +0100

    snack4

---
+++ snack-4/index.php
@@ -0,0 +1,7 @@
+<!-- Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi. 
+Ogni punto un nuovo paragrafo. -->
+
+<?php 
+    $paragraph = 'abcdefghi. jklmno. pqrst. uvwxyz';
+    var_dump(explode('.', $paragraph));
+?>
